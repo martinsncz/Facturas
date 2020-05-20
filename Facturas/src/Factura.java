cbe58f52562a2e48bea03b311bab6ad26274b50e
Enrique <enriquefernandezsantiago92@gmail.com>
2020-05-20 17:42:10 +0200

    Descomenta el atributo estados

--- Facturas/src/Factura.java
+++ Facturas/src/Factura.java
@@ -2,7 +2,7 @@ public class Factura {
 	public int num;
 	public float base;
 	public float tipoIva;
-	//public EStados estado;
+	public EStados estado;
 
 	public static void main(String[] args) {
 
