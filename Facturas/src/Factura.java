fec6c85be997835414e1c7671403acb2d7c4442b
Enrique <enriquefernandezsantiago92@gmail.com>
2020-05-20 17:50:08 +0200

    Implementacion de FacturasI en la clase Factura

--- Facturas/src/Factura.java
+++ Facturas/src/Factura.java
@@ -1,10 +1,19 @@
-public class Factura {
+public class Factura implements FacturaI {
 	public int num;
 	public float base;
 	public float tipoIva;
-	public EStados estado;
+	public Estados estado;
 
 	public static void main(String[] args) {
 
+	}
+
+	public void cambiarEstado(Estados estado) {
+	}
+
+	public float getTotal() {
+	}
+
+	public float getCantidadIva() {
 	}
 }
